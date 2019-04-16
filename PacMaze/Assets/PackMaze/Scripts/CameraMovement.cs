65f80306200e880fa3ee2e3cec22742c33314f55
KARMAPLAY <info@karmaplay.net>
2019-04-16 20:06:55 +0800

    [0.02] Added Player Movement and half-baked camera follow.

---
+++ PacMaze/Assets/PackMaze/Scripts/CameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Camera))]
+public class CameraMovement : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Vector3 offset;
+    public float smoothTime = 0.5f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    private Vector3 velocity;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if(targets.Count == 0)
+        {
+            return;
+        }
+
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = newPosition;
+    }
+
+
+    
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        // a virtual axis aligned bounding box, to get the center for the camera to look at
+        Bounds bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+}
---
+++ PacMaze/Assets/PackMaze/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿ using UnityEngine;
+
+// PlayerMovement script requires the Player's GameObject to have a Rigidbody component
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if(rb != null)
+        {
+            rb.constraints = RigidbodyConstraints.FreezePositionY;
+            rb.useGravity = false;
+        }
+    }
+
+    float d;
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h != 0 && v != 0)
+        {
+            // no diagnal movement
+            v = 0;
+        }
+
+        Vector3 movement = new Vector3(h, 0.0f, v);
+
+        rb.velocity = movement * speed;
+
+        //rb.MovePosition(transform.position + transform.forward * dir * Time.deltaTime);
+    }
+}
